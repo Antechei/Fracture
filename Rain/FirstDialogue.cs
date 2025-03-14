a51492280b2abf096ea7220d0d58bb5d7182bc47
Antechei <hanakerinotto@gmail.com>
2025-03-14 22:32:49 +1030

    Instructions bit and started Scene One

---
+++ Rain/FirstDialogue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain
+{
+    internal class FirstDialogue
+    {
+        string[] initialDialogue = {"Outside the window, a steady patter of rain begins.",
+        "it takes a few moments to build up, but the heavy droplets promise a small storm, at least.\n",
+        "The air is warm and humid for now, the heat still sloughing off the suburban concrete even with the sun settling behind the hills.",
+        "slowly, the air fills with the scent of warm, wet dirt. The droplets start to hit the ground faster than they can evaporate away.",
+        "Who are you?"};
+
+        string answersInitial = "[1] You feel calm as you breathe in the scent of rain. It's going to be a lovely night." +
+            "\n[2] You hug your arms against yourself despite the heat, dreading the stormy night to come." +
+            "\n[3] You hurry downstairs to close up the house against the humidity, grumbling under your breath." +
+            "\n[4] You rest your arms on the windowsill and sigh, listening to the patter of rain and wondering if they're doing the same.";
+
+        public void FirstScene()
+        {
+            Console.WriteLine();
+            Console.ReadKey(true);
+            Console.WriteLine(initialDialogue[0]);
+            Console.ReadKey(true);
+            Console.WriteLine(initialDialogue[1]);
+            Console.ReadKey(true);
+            Console.WriteLine(initialDialogue[2]);
+            Console.ReadKey(true);
+            Console.WriteLine(initialDialogue[3]);
+            Console.ReadKey(true);
+            Console.WriteLine();
+            Console.WriteLine(initialDialogue[4]);
+            Console.ReadKey(true);
+            Console.WriteLine();
+            Console.WriteLine(answersInitial);
+            Console.ReadKey(true);
+
+            Console.WriteLine("...");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine("\nWhoops, that's all for now! i spent way too long fucking around with the intro to write any content after this." +
+                "\nSo like, please go back and fuck with that a little. Please. Make it worth it-");
+            Console.ReadKey(true);
+        }
+
+        void SpaceInput()
+        {
+            char c;
+        }
+
+
+
+
+
+    }
+}
---
+++ Rain/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            FirstDialogue fD = new FirstDialogue();
+
+            void Instructions()
+            {
+                Console.WriteLine("\nWelcome to Rain.txt, aka my silly lil SlamJam2025 submission, aka a short text-based rpg about the rain, possibly?" +
+                    "\n\nPress Spacebar to continue to the next line.");
+                Console.ReadKey(true);
+                Console.WriteLine("\nGood job! You have a keyboard and reading comprehension, or else you got really lucky somehow I guess." +
+                    "\n\n(What the govenment doesn't want you to know is that any standard key input will work for now, but it'll be Spacebar later!" +
+                    "\n(Also, I recommend resizing this window so that each of these lines fits on the screen without wrapping! You should do that now)");
+                Console.ReadKey(true);
+                Console.WriteLine("\nAre you ready to progress? (Press the numerical key to select an option}\n[1] Yes.\n[2] No.");
+                
+                bool waiting = false;
+                bool waitingTwo = false;
+                bool waitingThree = false;
+                char i;
+                int oops = 0;
+                do
+                {
+                    i = Console.ReadKey(true).KeyChar;
+                    if (i == '1')
+                    {
+                        Console.Clear();
+                        if (waitingThree == true)
+                        {
+                            Console.WriteLine("\nThere you go, you did it!\nHave fun :)");
+                            Console.ReadKey(true);
+                            Console.Clear();
+                            return;
+                        }
+                        Console.WriteLine("\nHave fun.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        return;
+                    }
+                    else if (i == '2')
+                    {
+                        if (waiting == false)
+                        {
+                            if (waitingThree == true)
+                            { Console.WriteLine("\nHey, you found 2! I'm so proud of you.\nGood luck finding 1! Whenever you're ready :)"); }
+                            else { Console.WriteLine("\nUhhh, okay. Well, whenever you're ready..."); }
+                            waiting = true;
+                        }
+                    }
+                    else
+                    {   
+                        waitingTwo = true;
+                        if (waiting == false)
+                        {
+                            switch (oops)
+                            {
+                                case 0:
+                                    Console.WriteLine("\nYeah, that's not a 1 or a 2. Try again?");
+                                    oops++;
+                                    break;
+                                case 1:
+                                    Console.WriteLine("Not quite. You can do it!");
+                                    waitingThree = true;
+                                    oops++;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("So close!!");
+                                    oops++;
+                                    break;
+                                case 3:
+                                    Console.WriteLine("I'll always believe in you!");
+                                    oops++;
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Just one more try!!");
+                                    oops++;
+                                    break;
+                                case 5:
+                                    Console.WriteLine("One..... more....!");
+                                    oops++;
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Uh.");
+                                    oops++;
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Alright, I'm patient! I can wait. You take as much time as you need to find 1 or 2. I'll be here. <3");
+                                    oops++;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                } while ((waiting == true) || (waitingTwo == true));
+            }
+
+            Instructions();
+            fD.FirstScene();
+        }
+       
+    }
+}
